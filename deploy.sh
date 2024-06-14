03a27daafda92c14fe1c06c3e00fc9247d570bdf
신희성 <vvwv3092@gmail.com>
2024-06-14 23:49:04 +0900

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,22 +4,22 @@ APP_NAME=hello-infra
 REPOSITORY=/home/ubuntu/
 
 echo "> Check the currently running container"
-CONTAINER_ID=$(docker ps -aqf "name=$APP_NAME")
+CONTAINER_ID=$(sudo docker ps -aqf "name=$APP_NAME")
 
 if [ -z "$CONTAINER_ID" ];
 then
   echo "> No such container is running."
 else
   echo "> Stop and remove container: $CONTAINER_ID"
-  docker stop "$CONTAINER_ID"
-  docker rm "$CONTAINER_ID"
+  sudo docker stop "$CONTAINER_ID"
+  sudo docker rm "$CONTAINER_ID"
 fi
 
 echo "> Remove previous Docker image"
-docker rmi "$APP_NAME"
+sudo docker rmi "$APP_NAME"
 
 echo "> Build Docker image"
-docker build -t "$APP_NAME" "$REPOSITORY"
+sudo docker build -t "$APP_NAME" "$REPOSITORY"
 
 echo "> Run the Docker container"
-docker run -d -p 8080:8080 --name "$APP_NAME" "$APP_NAME"
+sudo docker run -d -p 8080:8080 --name "$APP_NAME" "$APP_NAME"
