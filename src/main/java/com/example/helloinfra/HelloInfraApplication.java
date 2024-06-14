355ab872565ff99dc30f24e3dc67c7cf89d5b876
신희성 <vvwv3092@gmail.com>
2024-06-14 23:04:41 +0900

    init

---
+++ src/main/java/com/example/helloinfra/HelloInfraApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloinfra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloInfraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloInfraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/helloinfra/HelloInfraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloinfra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloInfraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
