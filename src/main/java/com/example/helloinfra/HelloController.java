d1cc31cd8f5d681bef1685e63c7f34448496bbd5
신희성 <vvwv3092@gmail.com>
2024-06-15 19:51:48 +0900

    Update api response

--- src/main/java/com/example/helloinfra/HelloController.java
+++ src/main/java/com/example/helloinfra/HelloController.java
@@ -7,7 +7,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     @GetMapping
-    public ResponseEntity<String> hello() {
-        return ResponseEntity.ok("HELLO");
+    public ResponseEntity<Dto> hello() {
+        return ResponseEntity.ok(new Dto("HELLO"));
+    }
+}
+
+class Dto {
+    private String message;
+
+    public Dto(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
     }
 }
